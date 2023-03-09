dd2f88e0d32671ce33b0123c4e430362363e2cc6
Daniel Evensen <daevense@stud.ntnu.no>
2023-03-10 00:03:58 +0100

    Removed redundant code gotten from initializing project from inspiration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,7 +34,7 @@
           </v-col>
           <v-col cols="5">
             <div style="position: relative; z-index: 9999" class="mt-16">
-              <v-img src="i1.png" contain max-height="300"></v-img>
+              <v-img src="@/assets/portrait.png" contain max-height="300"></v-img>
             </div>
           </v-col>
         </v-row>
@@ -120,40 +120,13 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
 import NavBar from "../components/NavBar.vue";
 
 // Components
 //import HelloWorld from '../components/HelloWorld.vue';
 
-export default defineComponent({
+export default ({
   name: "HomeView",
-  setup() {
-    return {
-      slider2: 50,
-
-      items: [
-        {
-          img: "i3.jpg",
-        },
-        {
-          img: "i4.jpg",
-        },
-        {
-          img: "i5.jpg",
-        },
-        {
-          img: "i6.jpeg",
-        },
-        {
-          img: "i9.jpg",
-        },
-        {
-          img: "i8.jpg",
-        },
-      ],
-    };
-  },
   components: {
     NavBar,
 },
@@ -193,44 +166,4 @@ export default defineComponent({
   background: black;
   transform: skew(0deg, -6deg);
 }
-.egg {
-  display: block;
-  margin-left: 100px;
-  margin-top: 50px;
-  width: 356px;
-  height: 300px;
-  background-color: #fbdf7e;
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-}
-.first {
-  width: 100%;
-  height: 280px;
-  text-align: center;
-  padding: 2rem 2rem;
-}
-.child {
-  display: inline-block;
-  padding: 2rem 1rem;
-  vertical-align: middle;
-  text-align: center;
-  margin-right: 8px;
-}
-.imgHover {
-  padding: 0 200px;
-}
-.pre {
-  width: 100%;
-  height: 380px;
-  text-align: center;
-  padding: 0 200px;
-  background-color: #f5f5f5;
-}
-.hire {
-  width: 100%;
-  height: 200px;
-  padding: 0 200px;
-  background-color: #e9e9e9;
-  margin-top: -24px;
-}
-
 </style>
