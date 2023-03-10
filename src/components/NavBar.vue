c3d9f5fc1185a4b5b220cb5163aad03b1a5a23f8
Daniel Evensen <daevense@stud.ntnu.no>
2023-03-10 16:54:07 +0100

    Added TODO comments

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,11 @@
         <v-btn >
             <v-icon color="purple" left class="mr-2">fas fa-signature</v-icon>Daniel
         </v-btn>
-        <v-spacer></v-spacer>
+        <v-spacer>
+            <h1 class="text-purple" style="padding-left: 20%;" >SITE IS UNDER CONSTRUCTION</h1>
+        </v-spacer>
         <!--TODO: Make the color change depending on which section of the page user is at-->
+        <!--TODO: Make the buttons and navbar scaleable according to size of screen-->
         <v-btn text @click="scroll('home')" class="text-purple">Home</v-btn>
         <v-btn text @click="scroll('about')">About</v-btn>
         <v-btn text @click="scroll('resume')">Resume</v-btn>
