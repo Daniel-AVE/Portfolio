d8d7a4b534fbe6584eb459cba77e5569bd6d7cd9
Daniel Evensen <daevense@stud.ntnu.no>
2023-03-10 20:14:43 +0100

    Removed black bar with 'under construction' text from NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,16 @@
 <template>
-    <v-app-bar app color="black" dark flat class="px-12" fixed>
+    <v-app-bar app color="black" dark flat class="px-12" id="bar" fixed>
         <v-btn >
             <v-icon color="purple" left class="mr-2">fas fa-signature</v-icon>Daniel
         </v-btn>
-        <v-spacer>
-            <h1 class="text-purple" style="padding-left: 20%;" >SITE IS UNDER CONSTRUCTION</h1>
-        </v-spacer>
+        <v-spacer></v-spacer>
         <!--TODO: Make the color change depending on which section of the page user is at-->
         <!--TODO: Make the buttons and navbar scaleable according to size of screen-->
-        <v-btn text @click="scroll('home')" class="text-purple">Home</v-btn>
-        <v-btn text @click="scroll('about')">About</v-btn>
-        <v-btn text @click="scroll('resume')">Resume</v-btn>
-        <v-btn text @click="scroll('portfolio')">Portfolio</v-btn>
-        <v-btn text @click="scroll('contact')">Contact</v-btn>
+        <v-btn id="btn" text @click="scroll('home')" class="text-purple">Home</v-btn>
+        <v-btn id="btn" text @click="scroll('about')">About</v-btn>
+        <v-btn id="btn" text @click="scroll('resume')">Resume</v-btn>
+        <v-btn id="btn" text @click="scroll('portfolio')">Portfolio</v-btn>
+        <v-btn id="btn" text @click="scroll('contact')">Contact</v-btn>
     </v-app-bar>
 </template>
 
@@ -28,4 +26,7 @@ export default {
 </script>
 
 <style>
+#bar {
+    overflow:auto;
+}
 </style>
