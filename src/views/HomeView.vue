03b634a4207e6591ff6b3ea8f2fa66874511172b
Daniel Evensen <daevense@stud.ntnu.no>
2023-03-19 21:46:54 +0100

    Removed buggy hamburger-menu. Will work more on it before pushing again

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -262,7 +262,13 @@ export default {
   background: black;
   transform: skew(0deg, -6deg);
 }
-
+.navbar-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9999;
+}
 
 .resume {
   background: lightgray;
